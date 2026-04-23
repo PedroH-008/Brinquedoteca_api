76cbccc3be5348c7b5ad69100db07d575aa7face
Pedro Henrique <pedrofeitosa008123@gmail.com>
2026-04-23 19:07:47 -0300

    feat: Implementando domain de emprestimo e brinquedo Criado arquivos de schema

---
+++ domain/brinquedo.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Brinquedo:
+    id: int
+    nome: str
+    categoria: str
+    faixa_etaria_minima: int
+    disponivel: bool = True
+
+    
+
---
+++ domain/emprestimo.py
@@ -0,0 +1,10 @@
+class Emprestimo:
+    def __init__(self, id: int, criança_id: int, brinquedo_id: int, datas: int, status: bool, multa: int):
+        self.id: id
+        self.criança_id: criança_id
+        self.brinquedo_id: brinquedo_id
+        self.datas: datas
+        self.status: status
+        self.multa: multa
+
+    
